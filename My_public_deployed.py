bea6fd6d6aefaee09ef9cd6e3dba1169e86bae94
Getasew <42133391+amsalugetasew@users.noreply.github.com>
2024-01-15 12:26:38 -0800

    Update My_public_deployed.py

--- My_public_deployed.py
+++ My_public_deployed.py
@@ -10,7 +10,7 @@ from streamlit_option_menu import option_menu
 CyberSecurity_LR = pickle.load(open('LogisticRegration.sav', 'rb'))
 
 bank_prediction = pickle.load(open('classifier.pkl', 'rb'))
-placement_prediction = pickle.load(open('linear_regression.sav', 'rb'))
+placement_prediction = pickle.load(open('linear_regression.pkl', 'rb'))
 # salary_prediction = pickle.load(open('classifierlr.h5', 'rb'))
 salary_prediction = pickle.load(open('salary_prediction.sav', 'rb'))
 
