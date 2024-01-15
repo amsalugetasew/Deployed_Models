6306660fc6fa51cc4e091b1f7316298a60c6df0b
Getasew <42133391+amsalugetasew@users.noreply.github.com>
2024-01-15 13:08:48 -0800

    Update My_public_deployed.py

--- My_public_deployed.py
+++ My_public_deployed.py
@@ -15,8 +15,6 @@ bank_prediction_ANN = pickle.load(open('classifier.pkl', 'rb'))
 # salary_prediction = pickle.load(open('classifierlr.h5', 'rb'))
 salary_prediction = pickle.load(open('salary_prediction.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
-
 
 # sidebar for navigation
 with st.sidebar:
@@ -28,7 +26,6 @@ with st.sidebar:
                            'Bank Note Prediction',
                            'ANN based Bank Note Prediction',
                            'Placement Prediction',
-                           'Parkinsons Prediction'],
                           icons=['activity','heart','person'],
                           default_index=0)
     
@@ -138,127 +135,6 @@ if (selected == 'Data Scientist Salary Prediction'):
           Data_Scientist_Salary_prediction = "Authentic or Positive"
         
     st.success(Data_Scientist_Salary_prediction)
-    # if st.button('Salary prediction Test Result'):
-    #     salary_predictions = salary_prediction.predict([[work_year, experience_level, employment_type, job_title,employee_residence, remote_ratio, company_location,company_size, salary_currency, salary_in_usd]])                          
-        
-    #     if (salary_predictions[0] == 1):
-    #       Data_Scientist_Salary_prediction = 'Salary'
-    #     else:
-    #       Data_Scientist_Salary_prediction = 'Their is something wrong'
-        
-    # st.success(Data_Scientist_Salary_prediction)
-        
-
-    
-
-# Parkinson's Prediction Page
-if (selected == "Parkinsons Prediction"):
-    
-    # page title
-    st.title("Parkinson's Disease Prediction using ML")
-    
-    col1, col2, col3, col4, col5 = st.columns(5)  
-    
-    with col1:
-        fo = st.text_input('MDVP:Fo(Hz)')
-        
-    with col2:
-        fhi = st.text_input('MDVP:Fhi(Hz)')
-        
-    with col3:
-        flo = st.text_input('MDVP:Flo(Hz)')
-        
-    with col4:
-        Jitter_percent = st.text_input('MDVP:Jitter(%)')
-        
-    with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-        
-    with col1:
-        RAP = st.text_input('MDVP:RAP')
-        
-    with col2:
-        PPQ = st.text_input('MDVP:PPQ')
-        
-    with col3:
-        DDP = st.text_input('Jitter:DDP')
-        
-    with col4:
-        Shimmer = st.text_input('MDVP:Shimmer')
-        
-    with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-        
-    with col1:
-        APQ3 = st.text_input('Shimmer:APQ3')
-        
-    with col2:
-        APQ5 = st.text_input('Shimmer:APQ5')
-        
-    with col3:
-        APQ = st.text_input('MDVP:APQ')
-        
-    with col4:
-        DDA = st.text_input('Shimmer:DDA')
-        
-    with col5:
-        NHR = st.text_input('NHR')
-        
-    with col1:
-        HNR = st.text_input('HNR')
-        
-    with col2:
-        RPDE = st.text_input('RPDE')
-        
-    with col3:
-        DFA = st.text_input('DFA')
-        
-    with col4:
-        spread1 = st.text_input('spread1')
-        
-    with col5:
-        spread2 = st.text_input('spread2')
-        
-    with col1:
-        D2 = st.text_input('D2')
-        
-    with col2:
-        PPE = st.text_input('PPE')
-        
-    
-    
-    # code for Prediction
-    parkinsons_diagnosis = ''
-    
-    # creating a button for Prediction    
-    if st.button("Parkinson's Test Result"):
-        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,
-                                                           APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
-        
-        if (parkinsons_prediction[0] == 1):
-          parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
-          parkinsons_diagnosis = "The person does not have Parkinson's disease"
-        
-    st.success(parkinsons_diagnosis)
-
-    
-    
-    
-        
-#     # code for Prediction
-#     placement = ''
-#     # Bank Note Prediction
-#     # creating a button for Prediction    
-#     if st.button("Placement Test Result"):
-#         placement_prediction = bank_prediction.predict([[cgpa, id]])                          
-        
-#         if (placement_prediction[0] == 1):
-#           placement = "Not Placed or Negative"
-#         else:
-#           placement = "Placed or Positive"
-        
-#     st.success(placement)
 
 # Bank Note Authentication Prediction Page
 if (selected == 'Bank Note Prediction'):
